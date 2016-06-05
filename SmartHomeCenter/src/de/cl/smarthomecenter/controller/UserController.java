8719a1fc683b7d7007a915198d0d2dcf3077c273
Salzsack <salzsack@alpenjodel.de>
2016-06-05 17:50:13 +0200

    LoginDummy implemented; Primefacse included.

--- SmartHomeCenter/src/de/cl/smarthomecenter/controller/UserController.java
+++ SmartHomeCenter/src/de/cl/smarthomecenter/controller/UserController.java
@@ -1,5 +1,63 @@
+/**
+ class: UserController
+ Task: Controller to manage user inputs
+ Last modified: 05.06.2016
+ copyright by cl @ 2016 
+ **/
 package de.cl.smarthomecenter.controller;
 
-public class UserController {
+import java.io.IOException;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
+import de.cl.smarthomecenter.model.UserBean;
+
+@ManagedBean(name="userController")
+@SessionScoped
+public class UserController implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String enteredUsername = "";
+	private String enteredPassword = "";
+	private String userPermissions = "";
+	
+	@ManagedProperty(value="#{userBean}")
+	private UserBean userBean;
+		
+	public void setUserBean(UserBean userBean) {
+		this.userBean = userBean;
+	}
+	public String getEnteredUsername() {
+		return enteredUsername;
+	}
+	public void setEnteredUsername(String enteredUsername) {
+		this.enteredUsername = enteredUsername;
+	}
+	public String getEnteredPassword() {
+		return enteredPassword;
+	}
+	public void setEnteredPassword(String enteredPassword) {
+		this.enteredPassword = enteredPassword;
+	}
+	public String getUserPermissions() {
+		return userPermissions;
+	}
+	public void setUserPermissions(String userPermissions) {
+		this.userPermissions = userPermissions;
+	}
+	
+	public String checkCredentials(){
+		
+		if(true){
+			try {
+				FacesContext.getCurrentInstance().getExternalContext().redirect("shc.xhtml");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return "success";
+	}
 }
--- SmartHomeCenter/src/de/cl/smarthomecenter/model/UserBean.java
+++ SmartHomeCenter/src/de/cl/smarthomecenter/model/UserBean.java
@@ -1,5 +1,33 @@
 package de.cl.smarthomecenter.model;
 
-public class UserBean {
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
+@ManagedBean(name="userBean")
+@SessionScoped
+public class UserBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private String username = "";
+	private String password = "";
+	private String permissions = "";
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPermissions() {
+		return permissions;
+	}
+	public void setPermissions(String permissions) {
+		this.permissions = permissions;
+	}
 }
